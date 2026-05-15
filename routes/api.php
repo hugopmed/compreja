8b6480a9bf34a9b7efed21b61e71d8e98bc854ae
Túlio Gomes <tuliogomes07@hotmail.com>
2026-05-15 20:19:42 -0300

    Implement ProdutosController with CRUD operations and update API routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutosController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProdutosController::class, 'index']);
+    Route::post('/', [ProdutosController::class, 'store']);
+    Route::get('/{id}', [ProdutosController::class, 'show']);
+    Route::put('/{id}', [ProdutosController::class, 'update']);
+    Route::delete('/{id}', [ProdutosController::class, 'destroy']);
+});
+
